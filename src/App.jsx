5e2df1bff66e9f932647570149c1688cb52dff3c
asthas0108 <asthas0108@gmail.com>
2025-04-04 21:26:07 +0530

    add file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import './App.css'
+import './App.css';
+import './index.css';
+import { Loading } from './components/Loading';
+import { useState } from 'react';
+import { Navbar } from './components/Navbar';
 
 function App() {
 
+  const [isLoaded, setIsLoaded] = useState(false);
+
   return (
     <>
-
+      {!isLoaded && <Loading onComplete = {() => setIsLoaded(true)}/>}
+        <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
+        <Navbar/>
+        </div>
     </>
   )
 }
