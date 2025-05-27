a4029317e0a5b225301b81391af635cf1dcd9271
asthas0108 <asthas0108@gmail.com>
2025-05-27 19:49:31 +0530

    add files

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import './index.css';
 import { Loading } from './components/Loading';
 import { useState } from 'react';
 import { Navbar } from './components/Navbar';
+import { MobileMenu } from './components/MobileMenu';
+import { Home } from './components/sections/Home';
+import { About } from './components/sections/About';
+import { Projects } from './components/sections/Projects';
+import { Contact } from './components/sections/Contact';
 
 function App() {
 
@@ -16,6 +21,10 @@ function App() {
 
           <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
           <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+          <Home/>
+          <About/>
+          <Projects/>
+          <Contact/>
         </div>
     </>
   )
